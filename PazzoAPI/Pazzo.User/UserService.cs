948b8b37d76b89eb2ad3534e85acefbd8eff1c2d
宗澔 李 <lookliverpool@gmail.com>
2019-08-29 22:42:59 +0800

    建立CRUD的介面跟實作

---
+++ PazzoAPI/Pazzo.User/UserService.cs
@@ -0,0 +1,106 @@
+﻿namespace Pazzo.User
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class UserService
+    {
+        private log4net.ILog log = log4net.LogManager.GetLogger("UserService");
+        public DB.User GetUserById(Guid userId)
+        {
+            using (var db = new DB.PazzoDatabaseEntities())
+            {
+                return db.User.Where(c => c.Id == userId).FirstOrDefault();
+            }
+        }
+        public bool UpdateUser(DB.User user)
+        {
+            Trace.Write("Start Update User");
+            Trace.Write(string.Format("Data:{0}", JsonConvert.SerializeObject(user)));
+            if(user == null || user.Id == Guid.Empty)
+            {
+                return false;
+            }
+            try
+            {
+                using (var db = new DB.PazzoDatabaseEntities())
+                {
+                    var oldData = db.User.Where(c => c.Id == user.Id).FirstOrDefault();
+                    if (oldData != null)
+                    {
+                        db.Entry(oldData).CurrentValues.SetValues(user);
+                    }
+                    db.SaveChanges();
+                    Trace.Write("End Update User");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                Trace.Write("End Update User");
+                return false;
+            }
+        }
+        public bool CreateUser(DB.User user)
+        {
+            Trace.Write("Start Create User");
+            Trace.Write(string.Format("Data:{0}", JsonConvert.SerializeObject(user)));
+            if (user == null || user.Id == Guid.Empty)
+            {
+                return false;
+            }
+            try
+            {
+                using (var db = new DB.PazzoDatabaseEntities())
+                {
+                    var oldData = db.User.Where(c => c.Id == user.Id).FirstOrDefault();
+                    if (oldData == null)
+                    {
+                        db.User.Add(user);
+                    }
+                    db.SaveChanges();
+                    Trace.Write("End Create User");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                Trace.Write("End Create User");
+                return false;
+            }
+        }
+        public bool DeleteUserById(Guid userId)
+        {
+            Trace.Write("Start Delete User");
+            Trace.Write(string.Format("Data:{0}", userId));
+            if (userId == Guid.Empty)
+            {
+                return false;
+            }
+            try
+            {
+                using (var db = new DB.PazzoDatabaseEntities())
+                {
+                    var oldData = db.User.Where(c => c.Id == userId).FirstOrDefault();
+                    if (oldData != null)
+                    {
+                        db.User.Remove(oldData);
+                    }
+                    db.SaveChanges();
+                    Trace.Write("End Delete User");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                Trace.Write("End Delete User");
+                return false;
+            }
+        }
+    }
+}
---
+++ PazzoAPI/PazzoAPI/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿namespace PazzoAPI.Controllers
+{
+    using Pazzo.DB;
+    using Pazzo.User;
+    using System;
+    using System.Web.Http;
+
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+        [HttpGet]
+        [Route("api/User/{userId}")]
+        public User User(Guid userId)
+        {
+            UserService service = new UserService();
+            return service.GetUserById(userId);
+        }
+        [HttpPut]
+        public bool UpdateUser(User user)
+        {
+            UserService service = new UserService();
+            return service.UpdateUser(user);
+        }
+        [HttpDelete]
+        public bool DeleteUserById(Guid userId)
+        {
+            UserService service = new UserService();
+            return service.DeleteUserById(userId);
+        }
+        [HttpPost]
+        public bool CreateUser(User user)
+        {
+            UserService service = new UserService();
+            return service.CreateUser(user);
+        }
+    }
+}
